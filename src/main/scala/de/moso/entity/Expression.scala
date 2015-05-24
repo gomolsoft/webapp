b8199344a1b2a9e40c940cee462653f2062fa554
sandro <sandro.sole@gmail.com>
2015-05-24 23:07:08 +0200

    Auf Scala

--- src/main/scala/de/moso/entity/Expression.scala
+++ src/main/scala/de/moso/entity/Expression.scala
@@ -8,12 +8,53 @@ trait ModuleTypes[T] {
   def value: T
 }
 
-case class ModuleNumber[T](var value: BigInt[T]) extends ModuleTypes[Number[T]] {
+case class ModuleInt(var value: Int) extends ModuleTypes[Int] {
+  implicit def intToNumb(moduleInt: ModuleInt): ModuleNumber = {
+    ModuleNumber(BigInt(moduleInt.value))
+  }
 }
 
-case class ModuleBool(var value: Boolean) extends ModuleTypes[Boolean] {
+case class ModuleDouble(var value: Double) extends ModuleTypes[Double] {
+  implicit def floatToNumb(moduleFloat: ModuleDouble): ModuleNumber = {
+    ModuleNumber(BigDecimal(moduleFloat.value))
+  }
 }
 
+case class ModuleNumber(var value: Number) extends ModuleTypes[Number]
+
+case class ModuleBool(var value: Boolean) extends ModuleTypes[Boolean]
 
 //val b = new ModuleBool(true);
 //val n = ModuleNumber(5.9)
+
+trait LogicEspression {
+  def and(expression: LogicEspression): LogicEspression
+
+  def or(expression: LogicEspression): LogicEspression
+}
+
+class LogicEspressionBlock(logicOperator: LogicOperator) extends LogicEspression {
+  def and(expression: LogicEspression): LogicEspression
+
+  :
+  {
+
+  }
+
+  def or(expression: LogicEspression): LogicEspression
+
+  :
+  {
+    null
+  }
+
+}
+
+class LogicOperator[M: ModuleTypes] {
+  def >(operator: LogicOperator[M]): LogicOperator[M]
+
+  def <(operator: LogicOperator[M]): LogicOperator[M]
+
+  def ==(operator: LogicOperator[M]): LogicOperator[M]
+}
+
